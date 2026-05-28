51cbc706739a328dfa92fd6943394bb926c1a745
Muhammad Mujeeb Akkbar <akkbarmm428@gmail.com>
2026-05-28 13:01:22 +0500

    add

--- frontend/src/api/groups.js
+++ frontend/src/api/groups.js
@@ -6,3 +6,8 @@ export const getGroup = (id) => client.get(`/groups/${id}`).then((r) => r.data)
 export const updateGroup = (id, data) => client.patch(`/groups/${id}`, data).then((r) => r.data)
 export const addMember = (groupId, userId) => client.post(`/groups/${groupId}/members`, { userId }).then((r) => r.data)
 export const removeMember = (groupId, userId) => client.delete(`/groups/${groupId}/members/${userId}`).then((r) => r.data)
+export const uploadGroupAvatar = (groupId, file) => {
+  const form = new FormData()
+  form.append('avatar', file)
+  return client.post(`/groups/${groupId}/avatar`, form, { headers: { 'Content-Type': 'multipart/form-data' } }).then((r) => r.data)
+}
